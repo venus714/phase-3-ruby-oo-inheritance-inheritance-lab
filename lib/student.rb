5937e0d4b0813dc301fee48e7bb548f399f5fbd4
Student <venus.kabera@student.moringaschool.com>
2023-02-13 19:18:06 +0300

    completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge
+    
+    def initialize
+        super
+        @knowledge = []
+    end
+    def learn add
+        @knowledge = add
+    end
+    
 
 end
